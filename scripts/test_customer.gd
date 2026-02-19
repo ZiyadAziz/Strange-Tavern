1fc644f0f4549838b939aedc604db8c258cf88b5
ZiyadAziz <ziyad.abdelaziz03@gmail.com>
2026-02-18 20:10:06 -0500

    Moved dev environment to desktop, also godot 4.6

--- scripts/test_customer.gd
+++ scripts/test_customer.gd
@@ -2,4 +2,4 @@ extends Area2D
 
 func _input_event(viewport: Viewport, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed():
-		print("Sprite Clicked!")
+		print("Sprite Clicked")
