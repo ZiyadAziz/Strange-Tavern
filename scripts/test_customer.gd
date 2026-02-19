1fc71a0c8f491c7d26f4241eb545963c9b2e9d83
ZiyadAziz <ziyad.abdelaziz03@gmail.com>
2026-02-19 12:44:38 -0500

    Updated the customer a bit

--- scripts/test_customer.gd
+++ scripts/test_customer.gd
@@ -1,5 +1,17 @@
 extends Area2D
 
+#These dont need to be edited 
+var dialogue := "Burger with ketchup and extra mustard please"
+var order_taken := false 
+
+#made up example, but the first index would 
+#be the menu item (burger), and the rest would be what
+#is on the burger itself, 1 is ketchup, 2 is mustard
+#There is 2 mustards since they wanted extra
+var order:= ["Burger", 1, 2, 2] 
+
 func _input_event(viewport: Viewport, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed():
 		print("Sprite Clicked!")
+		print(dialogue)
+		print(order)
