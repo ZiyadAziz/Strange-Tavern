df38551cd5462838117b74f69c48a932a002322b
ZiyadAziz <ziyad.abdelaziz03@gmail.com>
2026-02-20 18:02:32 -0500

    more comments

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -3,12 +3,19 @@ extends Node
 #this will be the order last clicked, important for the prepping of the food so variables dont get mixed together
 var highlighted_order:= -1
 
-var orders: Array[int] = []
+#this array will track the orders currently in play, it would look like [1,2,3,4] when full
+#when an order gets completed, it would be removed from the array so if order 2 gets done
+#then it looks like [1,3,4], then when a new customer's order is taken the new order would
+#be order 2 and it would look like [1,3,4,2]. add_order and complete_order do this.
+#The reason an order needs a number is so that it differentiates the orders when you 
+#are doing the prep stuff, and the number will also allow you to position the order on the 
+#lefthand side, so each order number has its spot (like cook serve delish)
+var orders: Array[int] = [] 
 var max_orders:= 4 #this can be any number, 4 just feels right, not too overwhelming while still giving the player a choice on which order to think about
 
-var score = 0
-@onready var customer_dialogue: Label = $CustomerDialogue
+var score = 0 #this should be the number of correct orders
 
+@onready var customer_dialogue: Label = $CustomerDialogue
 func update_dialogue(dialogue: String):
 	customer_dialogue.text = dialogue
 
