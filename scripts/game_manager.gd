60f9afa3a47bbdaeadc74ab75f19b3c036325a76
ZiyadAziz <ziyad.abdelaziz03@gmail.com>
2026-02-20 17:25:22 -0500

    improved the adding and removing order capabilities

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -3,13 +3,8 @@ extends Node
 #this will be the order last clicked, important for the prepping of the food so variables dont get mixed together
 var highlighted_order:= -1
 
-#when an order gets added, orders will be updated, maxed out orders would look like [1,2,3,4] or any variation
-#so if order 2 gets completed, orders would look like [1,3,4] then if a new order gets added it would then look
-#like [1,3,4,2] and the new order gets assigned the number 2, this stuff would probably be updated in add_order
-#I'm pretty sure this is a horrible and inefficient way to do this, but I'm not a godot expert sooooooooooooooooooo
-var orders:= []
+var orders: Array[int] = []
 var max_orders:= 4 #this can be any number, 4 just feels right, not too overwhelming while still giving the player a choice on which order to think about
-var current_number_of_orders:= 0
 
 var score = 0
 @onready var customer_dialogue: Label = $CustomerDialogue
@@ -17,11 +12,29 @@ var score = 0
 func update_dialogue(dialogue: String):
 	customer_dialogue.text = dialogue
 
-#This will probably be updated a little more to take in parameters i think? It might also need a return value, so that the order that calls this knows which order it is
+# Adds a new order
+# Returns the number assigned to the order so the order knows which order it is
+# Returns -1 if max orders reached (but idk if this will be relavent, just in case I added it)
 #Also I'm pretty sure this should be called in customer not in order
-func add_order():
-	current_number_of_orders += 1
+func add_order() -> int:
+	if orders.size() >= max_orders:
+		return -1
+	
+	# Find the lowest available number starting from 1
+	var new_number := 1
+	while new_number in orders:
+		new_number += 1
+	
+	orders.append(new_number)
+	return new_number
 
-#theres probably a similar thought process to the add order about it returning stuff or needing parameters etc, but I can't think rn
-func complete_order():
-	current_number_of_orders -= 1
+
+# Completes an order by number
+# Removes it from active orders
+func complete_order(order_number: int) -> void:
+	if order_number in orders:
+		orders.erase(order_number)
+		
+		# If the completed order was highlighted, reset it
+		if highlighted_order == order_number:
+			highlighted_order = -1
