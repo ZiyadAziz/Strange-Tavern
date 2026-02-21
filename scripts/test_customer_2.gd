e38943ea8ed9105009e4619dbdd71815afdad487
ZiyadAziz <ziyad.abdelaziz03@gmail.com>
2026-02-20 22:08:29 -0500

    order tickers now tween

--- scripts/test_customer_2.gd
+++ scripts/test_customer_2.gd
@@ -8,6 +8,7 @@ const OrderScene = preload("res://scenes/test_order.tscn")
 #These dont need to be edited 
 var dialogue := "Burger with mayo please"
 var order_taken := false 
+var order_number:int
 
 #made up example, but the first index would 
 #be the menu item (burger), and the rest would be what
@@ -26,10 +27,11 @@ func _input_event(viewport: Viewport, event: InputEvent, shape_idx: int) -> void
 
 func instantiate_order():
 	var new_order = OrderScene.instantiate()
-	
-	# Pass the order data to the order scene
-	new_order.setup(order)
+	order_number = game_manager.add_order()
 	
 	# Add it to the scene tree, not sure what should be adding it, doing the customer themselves so that the order can queuefree the customer
 	#game_manager.add_child(new_order)
 	self.add_child(new_order)
+	
+	# Pass the order data to the order scene
+	new_order.setup(order, order_number)
--- scripts/test_order.gd
+++ scripts/test_order.gd
@@ -5,9 +5,16 @@ var order_data := []
 #need to know which ordernumber it is for when we prep the food
 var order_number:int
 
-func setup(data):
+func setup(data, number):
 	order_data = data
-	print("Order received: ", order_data)
+	order_number = number
+	print("Order received: ", order_data, order_number)
+
+	# Button Starts offscreen and tweens right, all the orders should now appear on the left hand side
+	global_position.x = -1800
+	global_position.y = -700 + (200 * order_number)
+	var tween = create_tween()
+	tween.tween_property(self, "global_position:x", -1000, 0.2)
 	
 func _gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
