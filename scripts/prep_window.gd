9955ab3dce08f7f4730ec6b0d6236831bd4e93a6
ZiyadAziz <ziyad.abdelaziz03@gmail.com>
2026-02-22 02:41:35 -0500

    Prep_Window UI Complete

--- scripts/prep_window.gd
+++ scripts/prep_window.gd
@@ -2,27 +2,77 @@ extends Panel
 
 @onready var game_manager = get_node("/root/Game/GameManager")
 @onready var orders_manager = get_node("/root/Game/Orders")
+@onready var food_images = get_node("/root/Game/FoodImages")
 
 var player_order := []
 var customer_order := []
 var current_order_node = null
 var correct_order := false
 
-#dont know if theres a better way to do this button/ui system, but it should work soooooooo
 @onready var burger: Button = $Burger
-@onready var burrito: Button = $Burrito
-@onready var pancake: Button = $Pancake
-@onready var milkshake: Button = $Milkshake
 @onready var ketchup: Button = $Ketchup
-@onready var vanilla: Button = $Vanilla
 @onready var mustard: Button = $Mustard
-@onready var chocolate: Button = $Chocolate
+@onready var mayo: Button = $Mayo
+@onready var lettuce: Button = $Lettuce
+@onready var tomato: Button = $Tomato
+@onready var onion: Button = $Onion
+@onready var patty: Button = $Patty
+@onready var top_bun: Button = $Bun
+
+@onready var burrito: Button = $Burrito
+@onready var beef: Button = $Beef
+@onready var chicken: Button = $Chicken
+@onready var salsa: Button = $Salsa
+@onready var cheese: Button = $Cheese
+@onready var beans: Button = $Beans
+@onready var guac: Button = $Guac
+@onready var wrap: Button = $Wrap
+
+@onready var pancake: Button = $Pancake
+@onready var extra_cake: Button = $"Extra Cake"
+@onready var strawberry_cake: Button = $Strawberry_Cake
+@onready var chocolate_cake: Button = $Chocolate_Cake
+@onready var blueberry: Button = $Blueberry
+@onready var syrup: Button = $Syrup
+
+@onready var milkshake: Button = $Milkshake
+@onready var vanilla: Button = $Vanilla
+@onready var chocolate: Button = $Chocolate_Shake
+@onready var motor_oil: Button = $"Motor Oil"
+@onready var strawberry: Button = $Strawberry_Shake
+
 @onready var clear: Button = $Clear
 @onready var submit: Button = $Submit
 
+var imagePaths: Array[String] = ["res://assets/images/bottom_bun.png", "res://assets/images/topbun.png", "res://assets/images/patty.png", 
+"res://assets/images/mustard.png", "res://assets/images/ketchup.png", "res://assets/images/mayo.png",
+"res://assets/images/lettice.png", "res://assets/images/tomato.png", "res://assets/images/onions.png"]
+
+func _ready() -> void:
+	#burger
+	preload("res://assets/images/bottom_bun.png") #0
+	preload("res://assets/images/topbun.png") #1
+	preload("res://assets/images/patty.png") #2
+	preload("res://assets/images/mustard.png") #3
+	preload("res://assets/images/ketchup.png") #4
+	preload("res://assets/images/mayo.png") #5
+	preload("res://assets/images/lettice.png") #6
+	preload("res://assets/images/tomato.png") #7
+	preload("res://assets/images/onions.png") #8
+	
+	#burrito
+	
+	#pancake
+	
+	#milkshake
+
+
 func show_prep(order_data, order_node):
+	for child in food_images.get_children():
+		child.queue_free()
+	
 	var tween = create_tween()
-	tween.tween_property(self, "position:x", 400, 0.1)
+	tween.tween_property(self, "position:x", 300, 0.1)
 	
 	#setting params
 	current_order_node = order_node
@@ -30,6 +80,7 @@ func show_prep(order_data, order_node):
 	
 	#resets the player order and prep window
 	player_order = []
+	
 	burger.show()
 	burrito.show()
 	pancake.show()
@@ -37,14 +88,40 @@ func show_prep(order_data, order_node):
 	
 	ketchup.hide()
 	mustard.hide()
+	mayo.hide()
+	lettuce.hide()
+	tomato.hide()
+	onion.hide()
+	patty.hide()
+	top_bun.hide()
+	
+	beef.hide()
+	chicken.hide()
+	salsa.hide()
+	cheese.hide()
+	beans.hide()
+	guac.hide()
+	wrap.hide()
+	
+	extra_cake.hide()
+	strawberry_cake.hide()
+	chocolate_cake.hide()
+	blueberry.hide()
+	syrup.hide()
+	
 	vanilla.hide()
 	chocolate.hide()
+	motor_oil.hide()
+	strawberry.hide()
 	
 	clear.show()
 	submit.show()
 
 	
 func hide_prep():
+	for child in food_images.get_children():
+		child.queue_free()
+
 	var tween = create_tween()
 	tween.tween_property(self, "position:x", 1000, 0.1)
 	
@@ -87,7 +164,11 @@ func _on_submit_pressed() -> void:
 	
 	hide_prep() #this also resets a bunch of variables too 
 
+#TODO: ADD ALL THE NEW BUTTONS TO BE SHOWN/HIDDEN
 func _on_clear_pressed() -> void:
+	for child in food_images.get_children():
+		child.queue_free()
+	
 	player_order = []
 	burger.show()
 	burrito.show()
@@ -96,8 +177,31 @@ func _on_clear_pressed() -> void:
 	
 	ketchup.hide()
 	mustard.hide()
+	mayo.hide()
+	lettuce.hide()
+	tomato.hide()
+	onion.hide()
+	patty.hide()
+	top_bun.hide()
+	
+	beef.hide()
+	chicken.hide()
+	salsa.hide()
+	cheese.hide()
+	beans.hide()
+	guac.hide()
+	wrap.hide()
+	
+	extra_cake.hide()
+	strawberry_cake.hide()
+	chocolate_cake.hide()
+	blueberry.hide()
+	syrup.hide()
+	
 	vanilla.hide()
 	chocolate.hide()
+	motor_oil.hide()
+	strawberry.hide()
 	
 	clear.show()
 	submit.show()
@@ -111,9 +215,110 @@ func _on_burger_pressed() -> void:
 	
 	ketchup.show()
 	mustard.show()
+	mayo.show()
+	lettuce.show()
+	tomato.show()
+	onion.show()
+	patty.show()
+	top_bun.show()
+	
+	beef.hide()
+	chicken.hide()
+	salsa.hide()
+	cheese.hide()
+	beans.hide()
+	guac.hide()
+	wrap.hide()
+	
+	extra_cake.hide()
+	strawberry_cake.hide()
+	chocolate_cake.hide()
+	blueberry.hide()
+	syrup.hide()
 	
 	vanilla.hide()
 	chocolate.hide()
+	motor_oil.hide()
+	strawberry.hide()
+	
+	#var foodImage = Sprite2D.new()
+	#var tex = load(imagePaths[0])
+	#foodImage.texture = tex
+	#foodImage.scale = Vector2(1, 1)
+	#foodImage.position = Vector2(0, 0)
+	#foodImage.z_index = 10
+	#
+	#food_images.add_child(foodImage)
+
+func _on_burrito_pressed() -> void:
+	player_order.append("Burrito")
+	burger.hide()
+	burrito.hide()
+	pancake.hide()
+	milkshake.hide()
+	
+	ketchup.hide()
+	mustard.hide()
+	mayo.hide()
+	lettuce.hide()
+	tomato.hide()
+	onion.hide()
+	patty.hide()
+	top_bun.hide()
+	
+	beef.show()
+	chicken.show()
+	salsa.show()
+	cheese.show()
+	beans.show()
+	guac.show()
+	wrap.show()
+	
+	extra_cake.hide()
+	strawberry_cake.hide()
+	chocolate_cake.hide()
+	blueberry.hide()
+	syrup.hide()
+	
+	vanilla.hide()
+	chocolate.hide()
+	motor_oil.hide()
+	strawberry.hide()
+
+func _on_pancake_pressed() -> void:
+	player_order.append("Pancake")
+	burger.hide()
+	burrito.hide()
+	pancake.hide()
+	milkshake.hide()
+	
+	ketchup.hide()
+	mustard.hide()
+	mayo.hide()
+	lettuce.hide()
+	tomato.hide()
+	onion.hide()
+	patty.hide()
+	top_bun.hide()
+	
+	beef.hide()
+	chicken.hide()
+	salsa.hide()
+	cheese.hide()
+	beans.hide()
+	guac.hide()
+	wrap.hide()
+	
+	extra_cake.show()
+	strawberry_cake.show()
+	chocolate_cake.show()
+	blueberry.show()
+	syrup.show()
+	
+	vanilla.hide()
+	chocolate.hide()
+	motor_oil.hide()
+	strawberry.hide()
 
 func _on_milkshake_pressed() -> void:
 	player_order.append("Milkshake")
@@ -124,19 +329,27 @@ func _on_milkshake_pressed() -> void:
 	
 	ketchup.hide()
 	mustard.hide()
+	mayo.hide()
+	lettuce.hide()
+	tomato.hide()
+	onion.hide()
+	patty.hide()
+	top_bun.hide()
+	
+	beef.hide()
+	chicken.hide()
+	salsa.hide()
+	cheese.hide()
+	beans.hide()
+	guac.hide()
+	wrap.hide()
+	
+	extra_cake.hide()
+	strawberry_cake.hide()
+	chocolate_cake.hide()
+	syrup.hide()
 	
 	vanilla.show()
 	chocolate.show()
-	
-	
-func _on_ketchup_pressed() -> void:
-	player_order.append(1)
-
-func _on_mustard_pressed() -> void:
-	player_order.append(2)
-
-func _on_vanilla_pressed() -> void:
-	player_order.append(1)
-
-func _on_chocolate_pressed() -> void:
-	player_order.append(2)
+	motor_oil.show()
+	strawberry.show()
